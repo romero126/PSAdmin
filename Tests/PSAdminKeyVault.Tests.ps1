3996cda0a6af8c7339b22db0f6d49f636eea0ec0
romero126 <romero126@gmail.com>
2019-02-18 01:35:09 -0800

    Update Tests to to more accuratly reflect features

--- Tests/PSAdminKeyVault.Tests.ps1
+++ Tests/PSAdminKeyVault.Tests.ps1
@@ -8,46 +8,64 @@ describe -Name "PSAdminKeyVault" {
     Context "New-PSAdminKeyVault" {
         
         it -Name "Validate [POS] Create KeyVault" {
-            New-PSAdminKeyVault -VaultName "VaultTest"
-            Get-PSAdminKeyVault -VaultName "VaultTest" | Should -HaveCount 1
+            New-PSAdminKeyVault -VaultName "Vault_New"
+            Get-PSAdminKeyVault -VaultName "Vault_New" | Should -HaveCount 1
         }
         
         it "Validate [NEG] Create KeyVault with Duplicate Vaultame" {
-            { New-PSAdminKeyVault -VaultName "VaultTest" } | Should -Throw
+            { New-PSAdminKeyVault -VaultName "Vault_New" } | Should -Throw
         }
 
     }
 
     Context "Get-PSAdminKeyVault" {
+        it "Validate [POS] Get Null" {
+            Get-PSAdminKeyVault "Vault_GetNull" | Should -HaveCount 0
+        }
 
         it "Validate [POS] Get" {
-            Get-PSAdminKeyVault "VaultTest" | Should -HaveCount 1
+            New-PSAdminKeyVault -VaultName "Vault_Get"
+            Get-PSAdminKeyVault "Vault_Get" | Should -HaveCount 1
+        }
+
+        it "Validate [POS] Get Exact" {
+            New-PSAdminKeyVault -VaultName "Vault_Get_Exact"
+            Get-PSAdminKeyVault "Vault_Get_Exact" | Should -HaveCount 1
         }
 
         it "Validate [POS] Get Wildcard" {
-            New-PSAdminKeyVault -VaultName "VaultTest_Get1"
-            New-PSAdminKeyVault -VaultName "VaultTest_Get2"
-            Get-PSAdminKeyVault -VaultName "VaultTest_*" | Should -HaveCount 2
+            Get-PSAdminKeyVault -VaultName "Vault_Get*" | Should -HaveCount 2
         }
 
     }
 
     Context "Set-PSAdminKeyVault" {
+        #Set Item should be wildcard only.
         it "Validate [POS] Set Item" {
-            New-PSAdminKeyVault -VaultName "VaultTest_SetItem"
-            Set-PSAdminKeyVault -VaultName "VaultTest_SetItem" -Location "VaultTest_SetItem"
-            Get-PSAdminKeyVault -VaultName "VaultTest_SetItem" | ForEach-Object Location | Should -Be "VaultTest_SetItem"
+            New-PSAdminKeyVault -VaultName "Vault_Set"
+            Set-PSAdminKeyVault -VaultName "Vault_Set" -Location "Vault_SetValue"
+            Get-PSAdminKeyVault -VaultName "Vault_Set" | ForEach-Object Location | Should -Be "Vault_SetValue"
         }
     }
 
     Context "Remove-PSAdminKeyVault" {
-        it "Validate [POS] Remove Vault" {
-            New-PSAdminKeyVault -VaultName "VaultTest_RemoveItem"
-            Remove-PSAdminKeyVault -VaultName "VaultTest_RemoveItem" -Confirm:$false
-            Get-PSAdminKeyVault -VaultName "VaultTest_RemoveItem" | Should -BeNullOrEmpty
+
+        it "Validate [POS] Remove Vault Exact" {
+            New-PSAdminKeyVault -VaultName "Vault_Remove_Exact"
+            Remove-PSAdminKeyVault -VaultName "Vault_Remove_Exact" -Confirm:$False
+            Get-PSAdminKeyVault -VaultName "Vault_Remove_Exact" | Should -BeNullOrEmpty
+
         }
+        
+        it "Validate [POS] Remove Vault WildCard" {
+            New-PSAdminKeyVault -VaultName "Vault_Remove_WildCard1"
+            New-PSAdminKeyVault -VaultName "Vault_Remove_WildCard2"
+            Remove-PSAdminKeyVault -VaultName "Vault_Remove_Wild*" -Confirm:$false -Match
+            Get-PSAdminKeyVault -VaultName "Vault_Remove_Wild*" | Should -BeNullOrEmpty
+        }
+
         it "Validate [NEG] Remove NonExistant Vault" {
-            { Remove-PSAdminKeyVault -VaultName "VaultTest_RemoveItem" } | Should -Throw 
+            { Remove-PSAdminKeyVault -VaultName "Vault_Remove_Null" } | Should -Throw 
         }
 
     }
@@ -62,7 +80,8 @@ describe -Name "PSAdminKeyVault" {
 
     Context "Cleanup" {
         it "Cleanup" {
-            Remove-PSAdminKeyVault -VaultName "*" -Confirm:$false
+            Remove-PSAdminKeyVault -VaultName "*" -Confirm:$false -Match
+            Get-PSAdminKeyVault -VaultName "*" | Should -HaveCount 0
         }
     }
 
