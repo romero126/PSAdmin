654eee33ed1e5829c4f6542a0d3f658f8c87f108
romero126 <romero126@gmail.com>
2019-02-19 23:13:13 -0800

    Itterate on individual machines instead of wildcard and ConfirmImpact

--- Source/Machine/Remove-PSAdminMachine.Public.ps1
+++ Source/Machine/Remove-PSAdminMachine.Public.ps1
@@ -1,13 +1,18 @@
 function Remove-PSAdminMachine
 {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'High')]
     param(
         [Parameter(ValueFromPipeline, Position=0, ValueFromPipelineByPropertyName)]
         [System.String]$Name,
+        
         [Parameter(ValueFromPipelineByPropertyName)]
         [System.String]$Id,
+        
         [Parameter(ValueFromPipelineByPropertyName)]
-        [System.String]$SQLIdentity
+        [System.String]$SQLIdentity,
+
+        [Parameter()]
+        [Switch]$Match
     )
 
     begin
@@ -20,39 +25,44 @@ function Remove-PSAdminMachine
 
     process
     {
-        $DBQuery = @{
-            Database        = $Database
-            Keys            = $Script:PSAdminMachineSchema.DB.Table.KEY | ForEach-Object { $_ }
-            Table           = $Script:PSAdminMachineSchema.DB.Table.Name
-            InputObject     = [PSCustomObject]@{}
+        #Generate Parameters without CommonParameters and Match
+        $Params = @{}
+        $PSBoundParameters.GetEnumerator() | 
+            Where-Object Key -ne "Match" | 
+                Where-Object { @([System.Management.Automation.PSCmdlet]::OptionalCommonParameters + [System.Management.Automation.PSCmdlet]::CommonParameters) -NotContains $_.Key } |
+                    ForEach-Object {
+                        $Params.Add($_.Key, $_.Value)
+                    }
+
+        $Machines = Get-PSAdminMachine @Params -Exact:(!$Match)
+
+        if (!$Machines)
+        {
+            Cleanup
+            throw ($Script:PSAdminLocale.GetElementById("MachineNotFound").Value -f $Name)
         }
 
-        $HasKey = foreach ($Param in $PSBoundParameters.GetEnumerator()) {
-            if ($DBQuery.Keys -contains $Param.Key)
+        foreach ($Machine in $Machines)
+        {
+            if (!$PSCmdlet.ShouldProcess( ($Script:PSAdminLocale.GetElementById("MachineRemove").Value -f $Machine.Name) ))
             {
-                $true
-                break
+                continue;
             }
-        }
 
-        if (!$HasKey)
-        {
-            Cleanup
-            Throw "You must specify a valid Searchable Key example:'$Keys'"
-        }
+            $DBQuery = @{
+                Database        = $Database
+                Keys            = $Script:PSAdminMachineSchema.DB.Table.KEY | ForEach-Object { $_ }
+                Table           = $Script:PSAdminMachineSchema.DB.Table.Name
+                InputObject     = $Machine
+            }
 
-        foreach ($Param in $PSBoundParameters.GetEnumerator())
-        {
-            Add-Member -InputObject $DBQuery.InputObject -MemberType NoteProperty -Name $Param.Key -Value $Param.Value
-        }
+            $Result = Remove-PSAdminSQliteObject @DBQuery -Match:($Match)
 
-        $Result = Remove-PSAdminSQliteObject @DBQuery
-
-        if ($Result -eq -1)
-        {
-
-            Cleanup
-            Throw "Unable to remove Item"
+            if ($Result -eq -1)
+            {
+                Cleanup
+                throw New-PSAdminException -ErrorID ExceptionUpdateDatabase
+            }
         }
 
     }
