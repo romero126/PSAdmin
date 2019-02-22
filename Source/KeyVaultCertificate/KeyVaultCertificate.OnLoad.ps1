924eb6cec5f0fee062254b3e9748214e91cde442
romero126 <romero126@gmail.com>
2019-02-22 12:18:20 -0800

    Better Schema Handling for PSAdminKeyVaultCertificate

--- Source/KeyVaultCertificate/KeyVaultCertificate.OnLoad.ps1
+++ Source/KeyVaultCertificate/KeyVaultCertificate.OnLoad.ps1
@@ -1,25 +1,3 @@
 $Database = Connect-PSAdminSQlite @Script:PSAdminDBConfig
-
-$KeyVaultCertificate = [PSCustomObject]@{
-    Certificate             = [Char[]]""
-    KeyId                   = [String]""
-    SecretId                = [String]""
-    Thumbprint              = [String]""
-    RecoveryLevel           = [String]""
-    ScheduledPurgeDate      = [String]""
-    DeletedDate             = [String]""
-    Enabled                 = [String]""
-    Expires                 = [String]""
-    NotBefore               = [String]""
-    Created                 = [String]""
-    Updated                 = [String]""
-    Tags                    = [String]""
-    VaultName               = [String]""
-    Name                    = [String]""
-    Version                 = [String]""
-    Id                      = [String]""
-}
-
-$Null = New-PSAdminSQLiteTable -Database $Database -Table "PSAdminKeyVaultCertificate" -PSCustomObject $KeyVaultCertificate
-
+$Null = New-PSAdminSQLiteTable -Database $Database -Table $Script:KeyVaultCertificateConfig.TableName -PSCustomObject $Script:KeyVaultCertificateConfig.TableSchema
 Disconnect-PSAdminSQLite -Database $Database
