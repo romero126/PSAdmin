44dfd8bc16c9b12a7b131eb353ed1974c2049512
romero126 <romero126@gmail.com>
2019-02-22 12:17:55 -0800

    Better Schema handling for PSAdminKeyVaultCertificate Store

---
+++ Source/KeyVaultCertificate/TableSchema.Private.ps1
@@ -0,0 +1,35 @@
+class PSAdminKeyVaultCertificate
+{
+    [String] $Id;
+    [String] $VaultName;
+    [String] $Name;
+
+    [String] $Version;
+
+    [String] $Enabled;
+    [String] $DeletedDate;
+
+    [String] $RecoveryLevel;
+    
+    [Nullable[DateTime]] $Created;
+    [Nullable[DateTime]] $Updated;
+
+    [Nullable[DateTime]] $NotBefore;
+    [Nullable[DateTime]] $Expires;
+
+    [Nullable[DateTime]] $ScheduledPurgeDate;
+
+    [String] $SecretId;
+    [String] $KeyId;
+
+    [PSObject] $Certificate;
+    [String] $Thumbprint;
+    
+    [String] $Tags;
+}
+
+$Script:KeyVaultCertificateConfig = @{
+    TableName           = "PSAdminKeyVaultCertificate"
+    TableKeys           = ("VaultName", "Name", "Id", "Thumbprint")
+    TableSchema         = [PSAdminKeyVaultCertificate]@{}
+}
