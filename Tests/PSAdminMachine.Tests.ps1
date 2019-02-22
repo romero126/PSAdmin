f02abde404fc8ee42a381cfdd3d15580d7493259
romero126 <romero126@gmail.com>
2019-02-22 12:53:09 -0800

    Update Tests to include VaultName

--- Tests/PSAdminMachine.Tests.ps1
+++ Tests/PSAdminMachine.Tests.ps1
@@ -4,36 +4,38 @@ describe "PSAdminMachine" {
     BeforeAll {
         Import-Module $PSScriptRoot\..\Module\PSAdmin\PSAdmin.psm1 -Force
         Open-PSAdmin -Path "$PSScriptRoot/TestDatabase/DBConfig.xml"
+        $VaultName      = "Vault_Machine_Test"
+        New-PSAdminKeyVault -VaultName $VaultName
     }
 
     Context "New-PSAdminMachine" {
         it "Validate [POS] Create Machine" {
-            New-PSAdminMachine -Name "Machine_New"
-            Get-PSAdminMachine -Name "Machine_New" | Should -HaveCount 1
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_New"
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_New" | Should -HaveCount 1
         }
 
         it "Validate [NEG] Create Machine with Duplicate Name" {
-            { New-PSAdminMachine -Name "Machine_New" } | Should -Throw
+            { New-PSAdminMachine -VaultName $VaultName -Name "Machine_New" } | Should -Throw
         }
     }
 
     Context "Get-PSAdminMachine" {
         it "Validate [POS] Get Null" {
-            Get-PSAdminMachine -Name "Machine_Get_Null" | Should -HaveCount 0
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Get_Null" | Should -HaveCount 0
         }
 
         it "Validate [POS] Get" {
-            New-PSAdminMachine -Name "Machine_Get"
-            Get-PSAdminMachine -Name "Machine_Get" | Should -HaveCount 1
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Get"
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Get" | Should -HaveCount 1
         }
 
         it "Validate [POS] Get Exact" {
-            New-PSAdminMachine -Name "Machine_Get_Exact"
-            Get-PSAdminMachine -Name "Machine_Get_Exact" | Should -HaveCount 1
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Get_Exact"
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Get_Exact" | Should -HaveCount 1
         }
 
         it "Validate [POS] Get Wildcard" {
-            Get-PSAdminMachine -Name "Machine_Get*" | Should -HaveCount 2
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Get*" | Should -HaveCount 2
         }
 
     }
@@ -41,37 +43,35 @@ describe "PSAdminMachine" {
     Context "Set-PSAdminMachine" {
         #Set Item should be wildcard only.
         it "Validate [POS] Set Value" {
-            New-PSAdminMachine -Name "Machine_Set"
-            Set-PSAdminMachine -Name "Machine_Set" -Description "Machine_Set"
-            Get-PSAdminMachine -Name "Machine_Set" | ForEach-Object Description | Should -Be "Machine_Set"
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Set"
+            Set-PSAdminMachine -VaultName $VaultName -Name "Machine_Set" -Description "Machine_Set"
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Set" | ForEach-Object Description | Should -Be "Machine_Set"
         }
     }
 
     Context "Remove-PSAdminMachine" {
         it "Validate [POS] Remove Exact" {
-            New-PSAdminMachine -Name "Machine_Remove_Exact"
-            Remove-PSAdminMachine -Name "Machine_Remove_Exact" -Confirm:$False
-            Get-PSAdminMachine -Name "Machine_Remove_Exact" | Should -BeNullOrEmpty
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_Exact"
+            Remove-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_Exact" -Confirm:$False
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_Exact" | Should -BeNullOrEmpty
 
         }
         
         it "Validate [POS] Remove WildCard" {
-            New-PSAdminMachine -Name "Machine_Remove_WildCard1"
-            New-PSAdminMachine -Name "Machine_Remove_WildCard2"
-            Remove-PSAdminMachine -Name "Machine_Remove_WildCard*" -Confirm:$false -Match
-            Get-PSAdminMachine -Name "Machine_Remove_WildCard*" | Should -BeNullOrEmpty
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_WildCard1"
+            New-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_WildCard2"
+            Remove-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_WildCard*" -Confirm:$false -Match
+            Get-PSAdminMachine -VaultName $VaultName -Name "Machine_Remove_WildCard*" | Should -BeNullOrEmpty
         }
 
         it "Validate [NEG] Remove NonExistant" {
-            { Remove-PSAdminKeyVault -Name "Machine_Remove_Null" } | Should -Throw 
+            { Remove-PSAdminKeyVault -VaultName $VaultName -Name "Machine_Remove_Null" } | Should -Throw 
         }
 
     }
 
     Context "Cleanup" {
-        Remove-PSAdminMachine -Name "*" -Confirm:$false -Match
-        Get-PSAdminMachine -Name "*" | Should -HaveCount 0
+        Remove-PSAdminMachine -VaultName $VaultName -Name "*" -Confirm:$false -Match
+        Get-PSAdminMachine -VaultName $VaultName -Name "*" | Should -HaveCount 0
     }
 }
-
-
