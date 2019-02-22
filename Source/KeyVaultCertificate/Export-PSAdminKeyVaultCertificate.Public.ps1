68a4e3589e77da8a9752d0a4c69387a6598efe95
romero126 <romero126@gmail.com>
2019-02-22 13:22:27 -0800

    Create Export-PSAdminKeyVaultCertificate

---
+++ Source/KeyVaultCertificate/Export-PSAdminKeyVaultCertificate.Public.ps1
@@ -0,0 +1,60 @@
+function Export-PSAdminKeyVaultCertificate
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ParameterSetName = "ExportToString")]
+        [Parameter(Mandatory, ParameterSetName = "ExportToFile")]
+        [System.String]$VaultName,
+
+        [Parameter(ParameterSetName = "ExportToString")]
+        [Parameter(ParameterSetName = "ExportToFile")]
+        [System.String]$ThumbPrint,
+
+        [Parameter(Mandatory, ParameterSetName = "ExportToFile")]
+        [System.String]$FilePath,
+        
+        [Parameter(Mandatory, ParameterSetName = "ExportToString")]
+        [Switch]$AsString,
+
+        [Parameter(Mandatory, ParameterSetName = "ExportToString")]
+        [Parameter(Mandatory, ParameterSetName = "ExportToFile")]
+        [SecureString]$Password
+        
+    )
+
+    begin
+    {
+
+    }
+
+    process
+    {
+        $Certificate = Get-PSAdminKeyVaultCertificate -VaultName $VaultName -ThumbPrint $ThumbPrint -Exact -Export
+
+        if (!$Certificate)
+        {
+            throw New-PSAdminException -ErrorID KeyVaultExceptionCertificateNotInstalled -ArgumentList $VaultName
+        }
+
+        $x509 = $Certificate.Certificate
+        $CertificateByteArray = $x509.Export( [System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $Password )
+
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            "ExportToFile" {
+                $CertificateByteArray | Set-Content -Path $FilePath -Encoding Byte
+            }
+            "ExportToString" {
+
+                [System.Convert]::ToBase64String($CertificateByteArray)
+            }
+        }
+
+    }
+
+    end
+    {
+
+    }
+
+}
