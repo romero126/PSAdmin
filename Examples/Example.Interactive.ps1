255dab57250336b9ff470bcb9945c7bb1cd6e5a6
romero126 <romero126@gmail.com>
2019-02-28 14:03:02 -0800

    Update Example Interactive

--- Examples/Example.Interactive.ps1
+++ Examples/Example.Interactive.ps1
@@ -18,44 +18,4 @@ Write-Host ""
 
 $AvailableCommands = Get-Command -Module "PSAdmin" | Sort-Object Noun | Group-Object Noun | Select @{n="Cmdlet#";e={ $_.Count } }, @{n='Noun';e={ $_.Name }}
 
-$AdailableCommands | ft -a
-
-New-PSAdminKeyVaultSecret -VaultName "Default" -Name "Test" -SecretValue "Waffles"
-
-<#
-$Pass = "MyPassword" | ConvertTo-SecureString -AsPlainText -Force
-Remove-PSAdminKeyVault -VaultName "Default"
-
-New-PSAdminKeyVault -VaultName "Default"
-Import-PSAdminKeyVaultCertificate -VaultName "Default" -Name "Default" -FilePath .\cert2.pfx -Password $Pass
-
-$Data = @{
-	Thumbprint		= Get-PSAdminKeyVaultCertificate -VaultName "Default" -Name "Default" | ForEach-Object Thumbprint
-	VaultKeyOrigional	= Get-PSAdminKeyVault -VaultName "Default" | ForEach-Object VaultKey
-	VaultKeyEncrypted	= "N/A"
-	
-}
-Write-Warning "Protecting Vault"
-Get-PSAdminKeyVault -VaultName "Default" | Protect-PSAdminKeyVault -Thumbprint $Data.Thumbprint
-$Data.VaultKeyEncrypted = Get-PSAdminKeyVault -VaultName "Default" | ForEach-Object VaultKey
-
-write-host $Data.Thumbprint
-try {
-    Write-Warning "Unprotecting Vault"
-    Get-PSAdminKeyVault -VaultName "Default" | UnProtect-PSAdminKeyVault -Thumbprint $Data.Thumbprint
-}
-catch {
-    Write-Error $_
-}
-$Data
-#>
-return
-
-$Data
-#Get-PSAdminKeyVault -VaultName "Default"
-return
-foreach($Command in $AvailableCommands)
-{
-    Write-Host ("{0}.  " -f ($AvailableCommands.IndexOf($Command)+1) ) -ForegroundColor Green -NoNewLine
-    Write-host $Command.Name -ForegroundColor Yellow
-}
+$AvailableCommands | Out-Host
