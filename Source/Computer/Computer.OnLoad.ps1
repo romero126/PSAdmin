d3bb6af1b7278961fb75865126d8aef56e5148f4
romero126 <romero126@gmail.com>
2019-03-05 23:33:19 -0800

    Remove Dependency on legacy code

--- Source/Computer/Computer.OnLoad.ps1
+++
@@ -1,3 +0,0 @@
-$Database = Connect-PSAdminSQlite @Script:PSAdminDBConfig
-$Null = New-PSAdminSQLiteTable -Database $Database -Table $Script:ComputerConfig.TableName -PSCustomObject $Script:ComputerConfig.TableSchema
-Disconnect-PSAdminSQLite -Database $Database
