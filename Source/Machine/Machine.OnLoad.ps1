1e6f2c57a835fb41996688abdd4d4d65a2ffcc9f
romero126 <romero126@gmail.com>
2019-03-05 22:28:25 -0800

    Removed References from PSAdminMachine

--- Source/Machine/Machine.OnLoad.ps1
+++
@@ -1,3 +0,0 @@
-$Database = Connect-PSAdminSQlite @Script:PSAdminDBConfig
-$Null = New-PSAdminSQLiteTable -Database $Database -Table $Script:MachineConfig.TableName -PSCustomObject $Script:MachineConfig.TableSchema
-Disconnect-PSAdminSQLite -Database $Database
