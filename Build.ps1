e19672505fc3fdcefb05243ece8a47d79af26ccc
romero126 <romero126@gmail.com>
2019-07-09 14:23:25 -0700

    Cleanup Build script

--- Build.ps1
+++ Build.ps1
@@ -1,11 +1,6 @@
 $ActionType = Split-Path -Path $MyInvocation.MyCommand.Path -Leaf
 Split-Path -Path $MyInvocation.MyCommand.Path -Leaf
 
-#$Config = Get-Content $PSScriptRoot/config.json | ConvertFrom-Json
-
-#$DrawLine = ("-" * 50)
-
-
 function Private:WriteAction
 {
     param(
@@ -19,40 +14,18 @@ function Private:WriteAction
         $Line = ("-" * 50)
     }
     Write-Host "`nExecuting Action: ${Action}\${Message}`n${Line}`n" -ForegroundColor Green
-    
-
 }
 
-#WriteAction("Build", "Cleanup-Files")
-#WriteAction "Build" "Cleanup-Files" $true
 
 
 #
 #Action: Build\Cleanup-Files
 #--------------------------------------------------
-WriteAction "Build" "Cleanup-Files"
+#WriteAction "Build" "Cleanup-Files"
 #Remove-Item $PSScriptRoot\Source\PSAdmin\obj -Recurse -ErrorAction 'SilentlyContinue' -Verbose
 
-#
-#Action: Build\Install-Dependencies
-#--------------------------------------------------
-WriteAction "Build" "Install-Dependencies"
-
-#dotnet add $PSScriptRoot\Source\PSAdmin package System.Management.Automation --version 6.2.0
-#dotnet add $PSScriptRoot\Source\PSAdmin package System.Management.Automation --version 6.1.0
-#dotnet add $PSScriptRoot\Source\PSAdmin package System.Management.Automation --version 6.0.0
-#dotnet add $PSScriptRoot\Source\PSAdmin package System.Data.SQLite --version 1.0.110
-#dotnet add $PSScriptRoot\Source\PSAdmin package System.Data.SQLite.Core --version 1.0.110
-#dotnet add package SQLite.Interop --version 1.0.0
 
 
-
-#
-#Action: Build\Build-Project
-#--------------------------------------------------
-WriteAction "Build" "Build-Project"
-#Write-Host ("`nExecuting Action: {0}\{1}`n{2}`n" -f "Build", "Build-Project", $DrawLine) -ForeGroundColor Green
-#dotnet build $PSScriptRoot\Source\PSAdmin
 
 #
 #Action: Build\Build-Module
@@ -63,22 +36,12 @@ New-Item -Path $PSScriptRoot/Module/PSAdmin -ItemType Directory -ErrorAction Sil
 Copy-Item "$PSScriptRoot/src/rc/*" -Recurse -Destination "$PSScriptRoot/Module/PSAdmin/" -Force
 
 dotnet publish -o "$PSScriptRoot\Module\PSAdmin\" --self-contained
-#start pwsh -Args "-noexit -command . { invoke-pester .\tests\PSAdmin.KeyVaultCertificate.Tests.ps1 }"
-start powershell -Args "-noexit -command . { invoke-pester .\tests\PSAdmin.KeyVaultCertificate.Tests.ps1 }"
-return
-
-
-
-
-
-
-
-
-
-
 
 #
 #Action: Build\Invoke-Pester
 #--------------------------------------------------
 WriteAction "Build" "Invoke-Pester"
-#Invoke-Pester $PSScriptRoot\
+WriteAction "Invoke-Pester" "Powershell"
+start powershell -Args "-command . .\Test.ps1" -NoNewWindow -Wait
+WriteAction "Invoke-Pester" "PWSH"
+start pwsh -Args "-command . .\Test.ps1" -NoNewWindow -Wait
